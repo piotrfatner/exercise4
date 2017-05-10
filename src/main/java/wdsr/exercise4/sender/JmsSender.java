e27b9808ad7b3b083d7920532444c2320bfb175a
piotrfatner <fatner.piotr@gmail.com>
2017-05-10 08:46:24 +0200

    jms sender basic jms

--- src/main/java/wdsr/exercise4/sender/JmsSender.java
+++ src/main/java/wdsr/exercise4/sender/JmsSender.java
@@ -1,20 +1,33 @@
 package wdsr.exercise4.sender;
 
+import java.io.Serializable;
 import java.math.BigDecimal;
+import java.util.Enumeration;
 import java.util.Map;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import sun.plugin2.message.EventMessage;
+import wdsr.exercise4.Order;
+
+import javax.jms.*;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import org.apache.activemq.ActiveMQConnectionFactory;
 
 public class JmsSender {
 	private static final Logger log = LoggerFactory.getLogger(JmsSender.class);
 	
 	private final String queueName;
 	private final String topicName;
+	private ActiveMQConnectionFactory connectionFactory;
+	private Connection connection;
+	private Session session;
 
 	public JmsSender(final String queueName, final String topicName) {
 		this.queueName = queueName;
 		this.topicName = topicName;
+		this.connectionFactory = new ActiveMQConnectionFactory("tcp://localhost:62616");
 	}
 
 	/**
@@ -24,7 +37,23 @@ public class JmsSender {
 	 * @param price Price of the product
 	 */
 	public void sendOrderToQueue(final int orderId, final String product, final BigDecimal price) {
-		// TODO
+		Order order = new Order(orderId,product,price);
+		try {
+			connection = connectionFactory.createConnection();
+			connection.start();
+			session = connection.createSession(false,Session.AUTO_ACKNOWLEDGE);
+			Destination destinationQueue = session.createQueue(queueName);
+
+			MessageProducer messageProducer = session.createProducer(destinationQueue);
+			ObjectMessage objectMessage = session.createObjectMessage(order);
+			objectMessage.setJMSType("Order");
+			objectMessage.setStringProperty("WDSR-System", "OrderProcessor");
+			messageProducer.send(objectMessage);
+			session.close();
+			connection.close();
+		} catch (JMSException e) {
+			e.printStackTrace();
+		}
 	}
 
 	/**
